faec116c18e93b2079590d9dd3f8133de2988343
choihaneul <qorfh317@gmail.com>
2020-07-15 19:25:03 +0900

    메인 gnb 수정, 서브 게시판, 폼 디자인 수정

--- src/main/header/header.js
+++ src/main/header/header.js
@@ -8,79 +8,73 @@ $(document).ready(function(){
         $(this).removeClass('on');
     });
 
-    var $header = $('#header');
-    var $gnb = $('#gnb');
-    var $gnb_li = $gnb.children('li');
-    var $gnb_li_a = $gnb_li.children('a');
-    var $gnb_li_ul = $gnb_li.children('ul');
-    var $gnb_li_ul_li_a = $gnb_li_ul.find('a');
-
-    var ht_arr = [];
-    var ht_1depth = $gnb_li_a.outerHeight();
-    var ht_max = 0;
-    var ht_header = $header.height();
-    var bgColor = $gnb_li_ul_li_a.css('background-color');
     var speed = 500;
-    var doneClose = true;
-    
-    getSubMaxHeight();
 
+    // 브라우저 로딩 시 rwd 호출
+    rwd();
 
-    //gnb영역에 마우스 오버시 2depth. bgGnb 보임
-    $header.on('mouseenter',openSub);    
-
-    //gnb영역에 마우스 아웃시 2depth, bgGnb안보임
-    $header.on('mouseleave', closeSub);  
-
-    //gnb 1depth a태그에 포커스(탭키) 이벤트 연결   
-    $gnb_li_a.on('focusin',openSub);    
-    $gnb_li.last().find('a').last().on('focusout',closeSub);	
-	
-	//1depth메뉴 활성화 유지    
-    $gnb_li.on('mouseenter',function(){
-        $(this).children('a').addClass('on');
-    });
-    $gnb_li.on('mouseleave',function(){
-        $(this).children('a').removeClass('on');
+    // 브라우저가 리사이즈 될 때마다 rwd( ) 호출
+    $(window).on('resize', function(){
+        rwd();
     });
 
-    function getSubMaxHeight(){
-        $gnb_li.each(function(i){
-            var current_ht = $(this).children('ul').height();
-            ht_max = Math.max(ht_max, current_ht);    
-			//ht_arr.push( $(this).children('ul').height() );
-            //ht_max = Math.max(ht_max, ht_arr[i]);			
-        });       
+   
+
+    // 브라우저 넓이 값에 따른 다른 이벤트 함수 연결
+    function rwd(){
+        var wid = $(window).width();
+
+        // 웹
+        if(wid >= 1000){
+            $("#gnbMo").removeClass("on");
+            $("#header").on("mouseenter", openSub);
+        
+            $("#header").on("mouseleave", closeSub);
+        }else{
+            // 타블릿 이하
+            // off() 있는 함수를 제거하는 함수
+            $("#header").off("mouseenter");
+        
+            $("#header").off("mouseleave");
+        }
     }
+    
+    // btnCall 클릭 시
+    $(".btnCall").on("click", function(){
+        var isOpen = $("#gnbMo").hasClass("on");
+
+        if(isOpen){
+            $("#gnbMo").removeClass("on");
+        }else{
+            $("#gnbMo").addClass("on")
+        }
+    });
+
+    $(".is-close-menu").on('click', function(){
+        console.log('cl')
+        $("#gnbMo").removeClass("on");
+    });
+
+    //gnbMo 2depth 보이기
+    $("#gnbMo>ul>li>a").on("click", function(e){
+        e.preventDefault();
+
+        var isOpen = $(this).next("ul").css("display");
+        if(isOpen == "block"){
+            $(this).next("ul").slideUp();
+        }
+        if(isOpen == "none"){
+            $(this).next("ul").slideDown();
+        }
+    });
 
     function openSub(){
-        var isBgGnb = $('.bgGnb').length;
-
-        if(!isBgGnb){
-            $header.prepend(
-                $('<div class="bgGnb">').css({
-                    width:'100%', height:ht_max,
-                    backgroundColor:bgColor,
-                    position:'absolute', top:ht_header, left:'0%',
-                    zIndex:'2', display:'none'
-                })
-            );
-        }  
-        
-        if(doneClose){
-            doneClose = false;
-            $gnb_li_ul.stop().slideDown(speed);
-            $('.bgGnb').stop().slideDown(speed);
-        }
-        
+        $("#gnb ul").stop().slideDown(speed);
+        $(".bgGnb").stop().slideDown(speed);
     }
-
     function closeSub(){
-        $gnb_li_ul.slideUp(speed-300);
-        $('.bgGnb').slideUp(speed,function(){
-            $(this).remove();
-            doneClose = true;
-        });
+        $("#gnb ul").slideUp(speed-300);
+        $(".bgGnb").slideUp(speed);
     }
 
 
